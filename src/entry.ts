ef20733fc2263f4a106d4d8c9a7c19e104e33417
Subhajit Sahu <wolfram77@gmail.com>
2020-06-13 16:09:23 +0530

    :watermelon: cool fruit +entry

---
+++ src/entry.ts
@@ -0,0 +1,11 @@
+import {value} from 'extra-array';
+
+/**
+ * Picks an arbitrary entry.
+ * @param x a set
+ * @param r random seed 0->1
+ */
+function entry<T>(x: Set<T>, r: number=Math.random()): [T, T] {
+  return value([...x.entries()], r);
+}
+export default entry;
