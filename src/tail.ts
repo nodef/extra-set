64dc8d7c7f505856b8aeba20f469b099227589d4
Subhajit Sahu <wolfram77@gmail.com>
2020-06-13 13:40:15 +0530

    :watermelon: cool fruit +tail

---
+++ src/tail.ts
@@ -0,0 +1,10 @@
+import drop from './drop';
+
+/**
+ * Gets set without the first value.
+ * @param x a set
+ */
+function tail<T>(x: Iterable<T>): Set<T> {
+  return drop(x, 1);
+}
+export default tail;
