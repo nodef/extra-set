60716d535cbb3c9ee7b46f0ff048efc4da199569
Subhajit Sahu <wolfram77@gmail.com>
2020-06-13 13:43:54 +0530

    :watermelon: cool fruit ;symmetricDifference

--- src/symmetricDifference.ts
+++ src/symmetricDifference.ts
@@ -1,19 +1,14 @@
-import union from './union';
-import tosets from './_tosets';
-
 /**
- * Gives a set with values in odd no. of sets.
- * @param xs sets
+ * Gives values not present in both sets.
+ * @param x a set
+ * @param y another set
  */
-function symmetricDifference<T>(...xs: Iterable<T>[]): Set<T> {
-  var s = union(...xs);
-  var ts = tosets(xs);
-  for(var v of s) {
-    var n = 0;
-    for(var t of ts)
-      if(t.has(v)) n++;
-    if((n & 1)===0) s.delete(v);
-  }
-  return s;
+function symmetricDifference<T>(x: Set<T>, y: Set<T>): Set<T> {
+  var a = new Set<T>();
+  for(var v of x)
+    if(!y.has(v)) a.add(v);
+  for(var v of y)
+    if(!x.has(v)) a.add(v);
+  return a;
 }
 export default symmetricDifference;
