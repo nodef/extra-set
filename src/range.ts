55de790e17ec5612c6e73968f70fce09f90a8526
Subhajit Sahu <wolfram77@gmail.com>
2020-06-13 14:39:31 +0530

    :watermelon: cool fruit ;range

--- src/range.ts
+++ src/range.ts
@@ -1,13 +1,25 @@
-import iterableRange from '@extra-iterable/range';
-import type {compareFn} from './_types';
+import id from './_id';
+import cmp from './_cmp';
+import type {compareFn, mapFn} from './_types';
 
 /**
- * Finds smallest and largest values.
+ * Finds smallest and largest entries.
  * @param x a set
- * @param fn compare function (a, b)
- * @returns [min, max]
+ * @param fc compare function (a, b)
+ * @param fm map function (v, v, x)
+ * @returns [smallest, largest]
  */
-function range<T>(x: Iterable<T>, fn: compareFn<T>=null): [T, T] {
-  return iterableRange(x, fn);
+function range<T, U>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): [[T, T], [T, T]] {
+  var fc = fc||cmp, fm = fm||id;
+  var mu: T, mv: T|U;
+  var nu: T, nv: T|U;
+  var i = 0;
+  for(var u of x) {
+    var v = fm(u, u, x);
+    if(i===0 || fc(v, mv)<0) { mu = u; mv = v; }
+    if(i===0 || fc(v, nv)>0) { nu = u; nv = v; }
+    i++;
+  }
+  return [[mu, mu], [nu, nu]];
 }
 export default range;
