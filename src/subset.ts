82c6f95f72c686eb120f7f03d9a03973df5fc7b8
Subhajit Sahu <wolfram77@gmail.com>
2020-06-13 13:47:31 +0530

    :watermelon: cool fruit ;subset

--- src/subset.ts
+++ src/subset.ts
@@ -1,27 +1,13 @@
-import arrayPermutation$ from '@extra-array/permutation-update';
-import random from './_random';
-
-function subsetNum<T>(x: Iterable<T>, n: number, r: number): Set<T> {
-  var a = arrayPermutation$(Array.from(x), n, r);
-  return new Set<T>(a);
-}
-
-function subsetAny<T>(x: Iterable<T>, r: number): Set<T> {
-  var rnd = random(r), a = new Set<T>();
-  for(var v of x)
-    if(rnd()>=0.5) a.add(v);
-  return a;
-}
+import {subsequence} from 'extra-array';
 
 /**
- * Gives an arbitrary subset.
+ * Picks an arbitrary subset.
  * @param x a set
  * @param n number of values (-1 => any)
  * @param r random seed 0->1
  */
-function subset<T>(x: Set<T>, n: number=-1, r: number=Math.random()): Set<T> {
-  var X = x.size;
-  if(n>=0) return n>X? null:subsetNum(x, n, r);
-  return subsetAny(x, r);
+function subobject<T>(x: Iterable<T>, n: number=-1, r: number=Math.random()): Set<T> {
+  var vs = subsequence([...x], n, r);
+  return new Set(vs);
 }
-export default subset;
+export default subobject;
--- src/subsets.ts
+++ src/subsets.ts
@@ -1,19 +1,12 @@
-import shift from './shift';
+import {subsequences} from 'extra-array';
 
 /**
- * Lists all subsets of a set.
+ * Lists all possible subsets.
  * @param x a set
  * @param n number of values (-1 => any)
  */
-function* subsets<T>(x: Set<T>, n: number=-1): IterableIterator<Set<T>> {
-  var X = x.size;
-  if(n>=X) { if(n==X) yield x; return; }
-  if(n===0 || X===0) { yield new Set(); return; }
-  var [a, y] = shift(x);
-  yield* subsets(y, n);
-  for(var s of subsets(y, n-1)) {
-    s.add(a);
-    yield s;
-  }
-}  
+function* subsets<T>(x: Iterable<T>, n: number=-1): IterableIterator<Set<T>> {
+  for(var vs of subsequences([...x], n))
+    yield new Set(vs);
+}
 export default subsets;
