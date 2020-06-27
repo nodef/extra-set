2e92e2a8863e11753bf4961bde4d8fe5eef608d8
Subhajit Sahu <wolfram77@gmail.com>
2020-06-27 15:48:11 +0530

    :coconut: chutney ;from

--- src/_types.ts
+++ src/_types.ts
@@ -1,8 +1,9 @@
-export type tillFn         = (dones: boolean[]) => boolean;
-export type combineFn<T>   = (a: T, b: T) => T;
-export type compareFn<T>   = (a: T, b: T) => number;
-export type calledFn<T>    = (v: T, k: T, x: Iterable<T>) => void;
-export type testFn<T>      = (v: T, k: T, x: Iterable<T>) => boolean;
-export type mapFn<T, U>    = (v: T, k: T, x: Iterable<T>) => T|U;
-export type reduceFn<T, U> = (acc: U, v: T, k: T, x: Iterable<T>) => U;
-export type getFn<T>       = () => T;
+export type arrayMapFn<T, U> = (v: T, i: number, x: Iterable<T>) => U;
+export type tillFn           = (dones: boolean[]) => boolean;
+export type combineFn<T>     = (a: T, b: T) => T;
+export type compareFn<T>     = (a: T, b: T) => number;
+export type calledFn<T>      = (v: T, k: T, x: Iterable<T>) => void;
+export type testFn<T>        = (v: T, k: T, x: Iterable<T>) => boolean;
+export type mapFn<T, U>      = (v: T, k: T, x: Iterable<T>) => T|U;
+export type reduceFn<T, U>   = (acc: U, v: T, k: T, x: Iterable<T>) => U;
+export type getFn<T>         = () => T;
--- src/from.ts
+++ src/from.ts
@@ -1,8 +1,12 @@
+import {from$} from 'extra-array';
+import type {arrayMapFn} from './_types';
+
 /**
- * Creates a set from iterable.
- * @param vs iterable
+ * Creates a set from values.
+ * @param vs values
+ * @param fm map function (v, i, x)
  */
-function from<T>(vs: Iterable<T>): Set<T> {
-  return new Set(vs);
+function from<T, U=T>(vs: Iterable<T>, fm: arrayMapFn<T, T|U>=null): Set<T|U> {
+  return new Set(fm? from$(vs).map(fm) : vs);
 };
 export default from;
